da1de86d550e3ceff46b15f90dd117cf76c0dbb4
jemy98 <51983671+jemy98@users.noreply.github.com>
2025-04-27 17:41:01 +0700

    Add files via upload
    add color picker

---
+++ color-picker/script.js
@@ -0,0 +1,9 @@
+document.getElementById("colorInput").addEventListener("input", function(event){
+    let selectedColor = event.target.value
+
+    document.getElementById("colorCode").textContent = selectedColor
+
+    document.getElementById("colorDisplay").style.backgroundColor = selectedColor
+
+
+})
