f7636fc9e7d64bcffea2b4b73c96f809020d774f
jemy98 <51983671+jemy98@users.noreply.github.com>
2025-04-27 17:42:58 +0700

    Delete color-picker directory

--- color-picker/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("colorInput").addEventListener("input", function(event){
-    let selectedColor = event.target.value
-
-    document.getElementById("colorCode").textContent = selectedColor
-
-    document.getElementById("colorDisplay").style.backgroundColor = selectedColor
-
-
-})
