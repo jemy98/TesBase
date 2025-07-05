e0a76968a8bf47cb2b177501749451d46f81ad86
jemy98 <51983671+jemy98@users.noreply.github.com>
2025-07-05 19:51:00 +0700

    Add files via upload

---
+++ guess-number/script.js
@@ -0,0 +1,6 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+const guessInput = document.getElementById('guessInput');
+const submitGuess = document.getElementById('submitGuess');
+const message = document.getElementById('message');
+const resetGame = document.getElementById('resetGame');
