1d4c8ef160d9dd7247922e0e055cd974f02e0996
jemy98 <51983671+jemy98@users.noreply.github.com>
2025-08-23 20:05:22 +0700

    Weather App

---
+++ weather-app/script.js
@@ -0,0 +1,4 @@
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const weatherIconElement = document.getElementById('weather-icon');
