15f482eee765293e4d72cb2c20a04d90abd72063
jemy98 <51983671+jemy98@users.noreply.github.com>
2025-08-30 20:49:05 +0700

    Typing Test

---
+++ typing-test/script.js
@@ -0,0 +1,6 @@
+const textToTypeElement = document.getElementById('text-to-type');
+const textToType = textToTypeElement.innerHTML.split(' ');
+const userInput= document.getElementById('user-input');
+const startButton = document.getElementById('start-button');
+cosnt timeDisplay = document.getElementById('time-display');
+const wpmDisplay = document.getElementById('wpm-display');
